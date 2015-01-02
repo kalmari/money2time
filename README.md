5e68d1752331e327ca9c963965488d1de6113d48
Yankovsky Andrey <YankovskyAndrey@gmail.com>
2015-01-02 11:37:54 +0530

    add README.md; add support for EUR; add support for "USD $20.32" format

---
+++ README.md
@@ -0,0 +1,28 @@
+TODO
+
+* Tests
+
+* different currencies
+
+* different formats
+
+* exceptions web sites
+
+* how to show prices and hours on one page
+
+* options page, where user can set his hour rate in any currency
+
+
+Formats that can into our app:
+
+* $12
+
+* $12.34
+
+* US $12
+
+* US $12.34
+
+* EUR 123
+
+* EUR 123,45
--- main.js
+++ main.js
@@ -1,11 +1,41 @@
-var dollarsPerHourRate = 20;
-var dollarRegexp = /\$(\d*\.?\d+)/g;
+function identityFunction(value) {
+    return value;
+}
+
+var rateInUsd = 20;
+
+var currencies = {
+    usd: {
+        exchangeRateToUsd: 1,
+        matches: /(US )?\$(\d*\.?\d+)/g,
+        matchingGroupIndex: 2
+    },
+    eur: {
+        exchangeRateToUsd: 1.21,
+        matches: /EUR (\d*,?\d+)/g,
+        normalizeValue: function(value) {
+            return value.replace(/,/, '.')
+        },
+        matchingGroupIndex: 1
+    }
+};
+
 var walk = document.createTreeWalker(document, NodeFilter.SHOW_TEXT, null, false);
 var node;
 while (node = walk.nextNode()) {
     if (node.parentNode.nodeName != 'SCRIPT') {
-        node.data = node.data.replace(dollarRegexp, function(match, p1) {
-            return (+p1 / dollarsPerHourRate).toFixed(2) + 'wh'
-        });
+        var replaced = false;
+        for (var currencyKey in currencies) {
+            var currency = currencies[currencyKey];
+            node.data = node.data.replace(currency.matches, function() {
+                replaced = true;
+                // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#Specifying_a_string_as_a_parameter
+                var value = (currency.normalizeValue || identityFunction)(arguments[currency.matchingGroupIndex]);
+                return (+value / currency.exchangeRateToUsd / rateInUsd).toFixed(2) + 'wh'
+            });
+            if (replaced) {
+                break;
+            }
+        }
     }
 }
